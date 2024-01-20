45dd40f10ac3a198af535a47b4355efb570a1c59
Georgii Khomaldi <khomaldi@hey.com>
2024-01-20 21:57:54 +0300

    Initial Commit

---
+++ Split Bill/Split_BillApp.swift
@@ -0,0 +1,17 @@
+//
+//  Split_BillApp.swift
+//  Split Bill
+//
+//  Created by Georgii Khomaldishvili on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Split_BillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
